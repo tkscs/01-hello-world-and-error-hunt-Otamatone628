095ea3523b7615455f88b66e7994c653131555f4
Ali <agordon2026@kehillah.org>
2025-08-28 09:03:58 -0700

    located errors

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# SyntaxError: unterminated string literal (detected at line 1)
+# NameError: name 'printeth' is not defined
+# NameError: name 'pleaseprint' is not defined
+# SyntaxError: invalid syntax
+print (2+2)
